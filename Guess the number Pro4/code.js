6d8762680ead9103db62af30e6b259f4f2e1949e
kashyap tiwari <kashyaptiwari444@gmail.com>
2026-08-21 16:35:07 +0530

    Add JavaScript practice projects

---
+++ Guess the number Pro4/code.js
@@ -0,0 +1,16 @@
+const randomNumber = parseInt(Math.random() * 100 + 1)  // generate random number
+
+const submit = document.querySelector('#subt');
+
+const  userInput = document.querySelector('#guessFeild');
+const  guessSlot= document.querySelector('.guesses');
+const  remaining = document.querySelector('.lastResult');
+const  lowOrHi = document.querySelector('.lowOrHi');
+const  startOver = document.querySelector('.resultParas');
+
+const p = document.createElement('p')
+
+let prevGuess = []
+let numGuess = 1
+
+let playGame = true
